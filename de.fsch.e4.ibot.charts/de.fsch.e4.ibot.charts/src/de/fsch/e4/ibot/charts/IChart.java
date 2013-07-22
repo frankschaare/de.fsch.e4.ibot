a322effd944efd18754ab1afebdd23e572e74aef
frankschaare <frank.schaare@gmail.com>
2013-07-22 02:11:19 +0200

    20130722: Candlestick begonnen

--- de.fsch.e4.ibot.charts/de.fsch.e4.ibot.charts/src/de/fsch/e4/ibot/charts/IChart.java
+++ de.fsch.e4.ibot.charts/de.fsch.e4.ibot.charts/src/de/fsch/e4/ibot/charts/IChart.java
@@ -3,11 +3,13 @@
  */
 package de.fsch.e4.ibot.charts;
 
+import de.fsch.e4.ibot.asset.ISecurity;
+
 /**
  * @author hit
  *
  */
 public interface IChart
 {
-
+public void setInput(ISecurity input);
 }
